c76fd8b90cf74f3b268366eedbedc619a1b5529b
Trismegiste <florent.g@allopneus.com>
2011-12-26 09:44:03 +0100

    refactoring : routing test on filter is a bad idea, now using on a controller

--- Command/GenerateRoutingTestCommand.php
+++ Command/GenerateRoutingTestCommand.php
@@ -15,55 +15,80 @@ use Symfony\Component\Routing\Matcher\Dumper\ApacheMatcherDumper;
  *
  * @author trismegiste@voila.fr
  */
-class GenerateRoutingTestCommand extends ContainerAwareCommand {
+class GenerateRoutingTestCommand extends ContainerAwareCommand
+{
 
     /**
      * @see Command
      */
-    protected function configure() {
-        $this
-                ->setDefinition(array(
-                    new InputArgument('filter', InputArgument::OPTIONAL, 'Filter for the routing', '.*'),
-                ))
-                ->setDescription('Generates a functional test class based on a collection of route filtered')
-                ->setHelp(<<<EOT
-The <info>test:generate:routing</info> command generates a functional test class based on a collection of route filtered.
+    protected function configure()
+    {
+        $this->
+            setDefinition(array(new InputArgument('controller', InputArgument::REQUIRED, 'Shortcut for controller')))->
+            setDescription('Generates a functional test class on a controller')->
+            setHelp("The <info>test:generate:routing</info> command generates a functional test class 
+                    based on a collection of route coming from a controller.\n\n
+                    <info>php app/console test:generate:routing AcmeBlogBundle:Front</info>")->
+            setName('test:generate:routing');
 
-<info>php app/console test:generate:routing [regexp]</info>
-EOT
-                )
-                ->setName('test:generate:routing')
-        ;
     }
 
     /**
      * @see Command
      */
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        $filter = $input->getArgument('filter');
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $controllerShortcut = $input->getArgument('controller');
         $router = $this->getContainer()->get('router');
+
+        list($bundle, $classController) = explode(':', $controllerShortcut);
+
+        $bundle = $this->getApplication()->getKernel()->getBundle($bundle);
+        $classNameController = $classController . 'Controller';
+        $fqcnController = $bundle->getNamespace() . '\Controller\\' . $classNameController;
 
         $routes = array();
         foreach ($router->getRouteCollection()->all() as $name => $route) {
-            if (preg_match("#$filter#", $name)) {
-                $testRoute = $route->compile();
+            $testRoute = $route->compile();
+            $def = $testRoute->getRoute()->getDefaults();
+            $ctrlRoute = explode('::', $def['_controller']);            
+            if ($fqcnController == $ctrlRoute[0]) {
+                list($ctrlRoute, $action) = $ctrlRoute;
                 $url = $testRoute->getPattern();
-                $url = preg_replace('#(\{([aA-zZ]+)\})#', '\$$2' , $url);
+                $url = preg_replace('#(\{([_aA-zZ]+)\})#', '\$$2', $url);
                 $requirements = $route->getRequirements();
-                if (!isset($requirements['_method'])) 
+                if (!isset($requirements['_method'])) {
                     $method = array('get');
-                else
-                    $method = (is_array($method)) ? $method : array($method);
-                
-                $routes[$name] = array(
-                    'url' => $url,
-                    'var' => $testRoute->getVariables(),
-                    'method' => $method
-                );
+                } else {
+                    $method = explode('|', strtolower($requirements['_method']));
+                }
+
+                $listVar = array();
+                foreach ($testRoute->getVariables() as $varName) {
+                    $listVar[$varName] = isset($requirements[$varName]) ? $requirements[$varName] : '*';
+                }
+
+                foreach ($method as $http) {
+                    $routes[$name . '_' . $http] = array('url' => $url, 'var' => $listVar, 'method' => $http);
+                }
             }
         }
 
-        var_dump($routes);
+        if (count($routes)) {
+            $filePath = sprintf("%s/Tests/Controller/%sTest.php", $bundle->getPath(), $classNameController);
+            var_dump($routes);
+            /*
+             $generator = new RoutingTestGenerator($this->get('filesystem'),  __DIR__.'/../Resources/skeleton/test');
+             $generator->generate($bundle, $routes);
+
+             $output->writeln(sprintf(
+             'The new %s.php class file has been created under %s.',
+             $generator->getClassName(),
+             $generator->getClassPath()
+             )); */
+        } else {
+            $output->writeln('No route found');
+        }
     }
 
 }
--- Generator/RoutingTestGenerator.php
+++ Generator/RoutingTestGenerator.php
@@ -9,12 +9,14 @@ use Symfony\Component\HttpKernel\Util\Filesystem;
  * Generates a test class based on a Doctrine entity.
  *
  */
-class RoutingTestGenerator extends SensioGenerator {
+class RoutingTestGenerator extends SensioGenerator
+{
 
     private $filesystem;
     private $skeletonDir;
- 
-    public function __construct(Filesystem $filesystem, $skeletonDir) {
+
+    public function __construct(Filesystem $filesystem, $skeletonDir)
+    {
         $this->filesystem = $filesystem;
         $this->skeletonDir = $skeletonDir;
     }
@@ -23,18 +25,17 @@ class RoutingTestGenerator extends SensioGenerator {
      * Generates the routing test class if it does not exist.
      *
      * @param array $collection
-     * @param string $filter 
+     * @param string $filter
      */
-    public function generate($collection, $filter) {
-        
+    public function generate($collection, $filter)
+    {
         if (file_exists($this->classPath)) {
             throw new \RuntimeException(sprintf('Unable to generate the %s test class as it already exists under the %s file', $this->className, $this->classPath));
         }
 
-        $this->renderFile($this->skeletonDir, 'RoutingTest.php.twig', $this->classPath, array(
-            'fqcnEntity' => $metadata->getName(),
-            'nudeValidation' => $this->getNudeObjectValidationError($metadata->getName()),
-        ));
+        $this->renderFile($this->skeletonDir, 'RoutingTest.php.twig', $this->classPath, 
+            array('fqcnEntity' => $metadata->getName(), 
+               'nudeValidation' => $this->getNudeObjectValidationError($metadata->getName())));
     }
 
 }
