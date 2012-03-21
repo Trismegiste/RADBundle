1f55cd5bfb7e30a39e891c9eb1bc682018ab8687
Florent Genette <florent.g@allopneus.com>
2012-03-21 16:53:17 +0100

    some comment

--- DependencyInjection/RoutingNameFilter.php
+++ DependencyInjection/RoutingNameFilter.php
@@ -7,12 +7,15 @@ use Symfony\Component\Routing\Route;
 /**
  * Description of RoutingFilter
  *
+ * Algorithm implementation of the Template Method :
+ *   Filtering based on a preg_match, think like a SQL "LIKE filter%"
+ *
  * @author flo
  */
 class RoutingNameFilter extends RoutingFilter {
 
     protected function isMatching($name, Route $route, $filter) {
-        return preg_match("#^$filter#", $name);
+        return preg_match("#^$filter#", $name);  // don't be fooled by the ^$ :)
     }
 
 }
