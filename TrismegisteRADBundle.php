1c49e56efa38ab79c44f8a5b0c082c5a9a6c2967
Florent <florent.g@allopneus.com>
2012-08-01 11:54:55 +0200

    removing extension : all work can be done in  bundle init

--- TrismegisteRADBundle.php
+++ TrismegisteRADBundle.php
@@ -3,7 +3,21 @@
 namespace Trismegiste\RADBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
 class TrismegisteRADBundle extends Bundle
 {
+
+    public function build(ContainerBuilder $container)
+    {
+        $container
+                ->register('routing.extract.ctrl', __NAMESPACE__ . '\DependencyInjection\RoutingControllerFilter')
+                ->addArgument(new Reference('router'));
+        $container
+                ->register('routing.extract.name', __NAMESPACE__ . '\DependencyInjection\RoutingNameFilter')
+                ->addArgument(new Reference('router'));
+    }
+
 }
